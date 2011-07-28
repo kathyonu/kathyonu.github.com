b0ea3ef8c6e998d0b0199b8dc0cd505e24a16988
kathyonu <iam@goodworksonearth.org>
2011-07-27 23:12:27 -0700

    removed all files except index.html

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-
-  def sequenceone(evt)
-   @sequenceone = evt[:sequence_text].to_textual
-   render :text
-  end
-
-end
--- app/models/anagram.rb
+++
@@ -1,5 +0,0 @@
-class Anagram < ActiveRecord::Base
-  attr_accessible :id, :anagram_text
-  belongs_to :sequence_created
-
-end
